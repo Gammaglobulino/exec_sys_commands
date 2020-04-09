0054c62d2b92adb4ed2fd0b5809c2cf2a870d6db
Gammaglobulino <mazzantia@hotmail.com>
2020-04-09 11:08:22 -0400

    complete async sending over the wire string lab

--- malaware_server_code/core/handle_connections/client_server_simulation_test.go
+++ malaware_server_code/core/handle_connections/client_server_simulation_test.go
@@ -3,110 +3,41 @@ package handle_connections
 import (
 	"../../../Execute_systems_commands"
 	"../handle_connections"
-	"bufio"
-	"fmt"
-	"net"
-	"strings"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestClientServerCommunicationLab(t *testing.T) {
-
-	var clientconn net.Conn
-	var serverconn net.Conn
-
-	conch := make(chan net.Conn)
-	canstart := make(chan bool)
-
 	localip, err := Execute_systems_commands.RetrieveCurrentIP()
-	if err != nil {
-		t.Fail()
-	}
+	assert.Nil(t, err)
 	localip = localip + handle_connections.Port
-
-	go handle_connections.MultiCoreListeningTo(&conch, &canstart, localip)
-	serverClosed := false
-	for {
-		select {
-		case serverconn = <-conch:
-			serverClosed = true
-			break
-		case isServerStarted := <-canstart:
-			if isServerStarted {
-				clientconn, err = handle_connections.ConnectTo(localip)
-				if err != nil {
-					t.Fatalf("Connection error %s", err.Error())
-					return
-				}
-			}
-			break
-		}
-		if serverClosed {
-			break
-		}
-	}
-	clientconn.Close()
-	serverconn.Close()
-}
-func TestSendStreamedDatatoClient(t *testing.T) {
-
-	var clientconn net.Conn
-	var serverconn net.Conn
-
-	conch := make(chan net.Conn)
-	canstart := make(chan bool)
-
-	localip, err := Execute_systems_commands.RetrieveCurrentIP()
-	if err != nil {
-		t.Fail()
-	}
-	localip = localip + handle_connections.Port
-
-	go handle_connections.MultiCoreListeningTo(&conch, &canstart, localip)
-	serverClosed := false
-	for {
-		select {
-		case serverconn = <-conch:
-			serverClosed = true
-			break
-		case isServerStarted := <-canstart:
-			if isServerStarted {
-				clientconn, err = handle_connections.ConnectTo(localip)
-				if err != nil {
-					t.Fatalf("Connection error %s", err.Error())
-					return
-				}
-			}
-			break
-		}
-		if serverClosed {
-			break
-		}
-	}
-
+	serverconn, clientconn, err := handle_connections.SetClientServerConnectionTo(localip)
+	assert.Nil(t, err)
+	assert.NotNil(t, serverconn)
+	assert.NotNil(t, clientconn)
 	defer clientconn.Close()
 	defer serverconn.Close()
+}
 
-	var (
-		data2send    string
-		dataReceived string
-	)
-	var numberofbytes int
+func TestClientServerCommunicationNotValidIp(t *testing.T) {
+	localip := "1.1.1.1:111"
+	_, _, err := handle_connections.SetClientServerConnectionTo(localip)
+	assert.NotNil(t, err)
+}
 
-	data2send = "Manu è culo\n"
-
-	numberofbytes, err = clientconn.Write([]byte(data2send))
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("Number of bytes written %v\n", numberofbytes)
-
-	readFromServer := bufio.NewReader(serverconn)
-	dataReceived, err = readFromServer.ReadString('\n')
-	if err != nil {
-		t.Fatal(err)
-	}
-	strings.TrimSuffix(dataReceived, "\n")
-	fmt.Printf("Data recieved from the server: %s", dataReceived)
-
+func TestSendStreamedDatatoClient(t *testing.T) {
+	localip, err := Execute_systems_commands.RetrieveCurrentIP()
+	assert.Nil(t, err)
+	localip = localip + handle_connections.Port
+	serverconn, clientconn, err := handle_connections.SetClientServerConnectionTo(localip)
+	assert.Nil(t, err)
+	assert.NotNil(t, serverconn)
+	assert.NotNil(t, clientconn)
+	defer clientconn.Close()
+	defer serverconn.Close()
+	data2send := "This is a string message\n"
+	numberofbytes, err := handle_connections.SendStringTo(clientconn, data2send)
+	assert.EqualValues(t, 25, numberofbytes)
+	dataReceived, err := handle_connections.ReceiveStringFrom(serverconn)
+	assert.EqualValues(t, "This is a string message\n", dataReceived)
 }
--- malaware_server_code/core/handle_connections/server_connection.go
+++ malaware_server_code/core/handle_connections/server_connection.go
@@ -1,9 +1,10 @@
 package handle_connections
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"net"
+	"strings"
 )
 
 func SingleThreadListeningto(ip string) (net.Conn, error) {
@@ -21,25 +22,70 @@ func SingleThreadListeningto(ip string) (net.Conn, error) {
 	}
 	return connection, nil
 }
-func MultiCoreListeningTo(connectionch *chan net.Conn, canstarttosend *chan bool, ip string) {
+func MultiCoreListeningTo(connectionch *chan net.Conn, canstarttosend *chan bool, errorch *chan error, ip string) {
 	var connection net.Conn
 	listener, err := net.Listen("tcp", ip)
 	if err != nil {
-		close(*connectionch)
-		close(*canstarttosend)
-		log.Fatal(err)
+		*errorch <- err
 	}
 	fmt.Println("Listening to:", ip)
 	*canstarttosend <- true //client can start to listen
 
 	connection, err = listener.Accept()
 	if err != nil {
-		close(*connectionch)
-		close(*canstarttosend)
-		log.Fatal(err)
+		*errorch <- err
 	} else {
 		fmt.Println("Connection established from:" + connection.RemoteAddr().String())
 	}
 	*connectionch <- connection
 
+}
+func SendStringTo(conn net.Conn, stringtoSend string) (int, error) {
+	bytes, err := conn.Write([]byte(stringtoSend))
+	if err != nil {
+		return 0, err
+	}
+	return bytes, nil
+}
+
+func ReceiveStringFrom(conn net.Conn) (string, error) {
+	readFromServer := bufio.NewReader(conn)
+	dataReceived, err := readFromServer.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	strings.TrimSuffix(dataReceived, "\n")
+	return dataReceived, nil
+}
+
+func SetClientServerConnectionTo(ip string) (server net.Conn, client net.Conn, err error) {
+
+	conch := make(chan net.Conn)
+	canstart := make(chan bool)
+	errorch := make(chan error)
+
+	go MultiCoreListeningTo(&conch, &canstart, &errorch, ip)
+	serverClosed := false
+	for {
+		select {
+		case server = <-conch:
+			serverClosed = true
+			break
+		case isServerStarted := <-canstart:
+			if isServerStarted {
+				client, err = ConnectTo(ip)
+				if err != nil {
+					return nil, nil, err
+				}
+			}
+			break
+		case error := <-errorch:
+			return nil, nil, error
+
+		}
+		if serverClosed {
+			break
+		}
+	}
+	return server, client, nil
 }
